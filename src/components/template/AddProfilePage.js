c55713a72d23bc48fce97434df0b9d8f1a10b31e
amirkhoda8 <74904393+amirkhoda@users.noreply.github.com>
2023-12-16 16:33:12 +0330

    add text input and text list and radio button and add funtionality for the all of the addProfile

---
+++ src/components/template/AddProfilePage.js
@@ -0,0 +1,83 @@
+import TextInput from "@/module/TextInput";
+import styles from "./AddProfilePage.module.css";
+import React, { useState } from "react";
+import RadioList from "@/module/RadioList";
+import TextList from "@/module/TextList";
+
+function AddProfilePage() {
+  const [profileData, setProfileData] = useState({
+    title: "",
+    description: "",
+    location: "",
+    phone: "",
+    price: "",
+    realState: "",
+    constructionDate: new Date(),
+    category: "",
+    rules: [],
+    amenities: [],
+  });
+  const submitHandler = () => {
+    console.log(profileData);
+  };
+  return (
+    <div className={styles.container}>
+      <h3>ثبت آگهی</h3>
+      <TextInput
+        title="عنوان آگهی:"
+        name="title"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />{" "}
+      <TextInput
+        title=" توضیحات:"
+        name="description"
+        profileData={profileData}
+        setProfileData={setProfileData}
+        textarea={true}
+      />
+      <TextInput
+        title="آدرس"
+        name="location"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+      <TextInput
+        title="شماره تماس"
+        name="phone"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+      <TextInput
+        title="قیمت(تومان)"
+        name="price"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+      <TextInput
+        title="بنگاه"
+        name="realState"
+        profileData={profileData}
+        setProfileData={setProfileData}
+      />
+      <RadioList profileData={profileData} setProfileData={setProfileData} />
+      <TextList
+        title="امکانات رفاهی"
+        profileData={profileData}
+        setProfileData={setProfileData}
+        type="amenities"
+      />{" "}
+      <TextList
+        title="قوانین"
+        profileData={profileData}
+        setProfileData={setProfileData}
+        type="rules"
+      />
+      <button className={styles.submit} onClick={submitHandler}>
+        ثبت آگهی
+      </button>
+    </div>
+  );
+}
+
+export default AddProfilePage;
